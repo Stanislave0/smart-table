5b379e283b6b96c3ca59ea57ea4b850b78a28ca4
Stanislave0 <stassot67@gmail.com>
2026-05-17 19:03:30 +0300

    Fixes filtering

--- src/components/searching.js
+++ src/components/searching.js
@@ -2,15 +2,18 @@ import {rules, createComparison} from "../lib/compare.js";
 
 
 export function initSearching(searchField) {
+    const searchKey = typeof searchField === 'string'
+        ? searchField
+        : searchField?.search?.name || searchField?.name || 'search';
+
     // @todo: #5.1 — настроить компаратор
     const comparator = createComparison(
         ['skipEmptyTargetValues'],
-        [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)]
+        [rules.searchMultipleFields(searchKey, ['date', 'customer', 'seller'], false)]
     );
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        // return data;
         return data.filter((row) => comparator(row, state));
     }
 }
